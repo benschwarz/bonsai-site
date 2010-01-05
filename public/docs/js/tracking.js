c826196be6f91a29f4095662233bd30467516371
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-06 00:26:21 +1100

    Added exit link tracking

---
+++ public/docs/js/tracking.js
@@ -0,0 +1,8 @@
+$(function() {
+	var as = $("a[@href^='http']");
+	for(var i=0; i < as.length; i++) {
+		$(as[i]).click(function() {
+			pageTracker._trackPageview('/outgoing/' + $(this).attr("href").replace('http://', ''));
+		});
+	}
+});
