ef19cb830bce240cf53f72f9bce9dab6afa81276
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-07 14:17:02 +1100

    Used a real jquery selector

--- public/docs/js/tracking.js
+++ public/docs/js/tracking.js
@@ -1,8 +1,5 @@
 $(function() {
-	var as = $("a[href^='http']");
-	for(var i=0; i < as.length; i++) {
-		$(as[i]).click(function() {
+	$("a[href^='http']").click(function() {
 			pageTracker._trackPageview('/outgoing/' + $(this).attr("href").replace('http://', ''));
-		});
-	}
+	});
 });
