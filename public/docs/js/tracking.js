50f35b730afa7b616b83626c5c342fda2e601d29
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-17 12:00:53 +1100

    Syncing with the last round of copy changes. Added page tracking.

--- public/docs/js/tracking.js
+++ public/docs/js/tracking.js
@@ -1,5 +1,29 @@
 $(function() {
+	
+	// Out links
 	$("a[href^='http']").click(function() {
 			pageTracker._trackPageview('/outgoing/' + $(this).attr("href").replace('http://', ''));
 	});
+	
+	// Download tracking
+	$("a[href$='pdf'], a[href$='zip']").click(function () {
+		pageTracker._trackEvent("Downloads", this.href.match(/.\w+$/), this.href)
+	});
+	
+	// Amount of page scrolled
+	// If the page can be scrolled, what percentage was scrolled?
+	$(window).scroll(function(){ 
+	  var bottom = $(window).height() + $(window).scrollTop(); 
+	  var height = $(document).height(); 
+	  var percentage = Math.round(100*bottom/height); 
+
+		pageTracker._trackEvent("UX", "Page scrolling", percentage);
+	});
+	
+	// Elements clicked that weren't A's ?? 
+	// $(":not(html,body,a)") ??
+	
+	// Tabbing through fields (Do users actually do it?)
+	
+	// Copy and paste tracking
 });
